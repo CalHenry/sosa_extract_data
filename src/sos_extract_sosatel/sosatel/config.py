efa94685ec7e8721526e784d6c1b55c6d08cdbf6
Calixte Henry <131826512+CalHenry@users.noreply.github.com>
2026-08-07 18:31:16 +0200

    refactor: moved script and config to subfolder
    adding extract code for another website -> move files to subfolders

--- src/sos_extract_sosatel/sosatel/config.py
+++ src/sos_extract_sosatel/sosatel/config.py
@@ -1,9 +1,10 @@
 from datetime import date
 import logging
 
-BASE_URL = "https://sosatel.sosamitie.org"
-STATS_PAGE_URL = f"{BASE_URL}/stats/histories/search"
-LOGIN_URL = f"{BASE_URL}/login"
+# SOSATEL
+SOSATEL_URL = "https://sosatel.sosamitie.org"
+STATS_PAGE_URL = f"{SOSATEL_URL}/stats/histories/search"
+LOGIN_URL = f"{SOSATEL_URL}/login"
 
 START_DATE = date(2026, 5, 13)
 END_DATE = date(2026, 7, 31)
